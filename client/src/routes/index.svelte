f1c34c9d5bb292217b7c636df4206182d2300ab9
mb961107 <mirko.blazina@convergent-usa.com>
2021-05-22 13:55:39 +0200

    svelte scss

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,2 +1,16 @@
+<script>
+
+</script>
+<style lang="scss">
+    h1{
+        color: red;
+        &::before{
+            content: ":) ";
+        }
+    }
+</style>
+<svelte:head>
+    <title>Homepage</title>
+</svelte:head>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
